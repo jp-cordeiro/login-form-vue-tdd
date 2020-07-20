f4bedb25483c2a7eee5c90a60329c53bff118d4f
jp-cordeiro <jpc.araujo@hotmail.com>
2020-07-19 22:52:58 -0300

    add vuelidate

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuelidate from "vuelidate";
 // import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuelidate);
+
 new Vue({
   // router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
