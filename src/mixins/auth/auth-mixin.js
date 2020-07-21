e5ca74c7c36ab358bca6613ab6f4b73758908aa3
Joao Araujo <joaoaraujo@JETRO.LOCAL>
2020-07-21 10:41:40 -0300

    add authMixin

---
+++ src/mixins/auth/auth-mixin.js
@@ -0,0 +1,31 @@
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      navigation: 'getNavigation',
+    }),
+    isValid() {
+      return !this.$v.$invalid;
+    },
+  },
+  methods: {
+    isAttrInvalid(attr) {
+      return this.$v.user[attr].$dirty && this.$v.user[attr].$invalid;
+    },
+    teste() {
+      console.log('mixin');
+    },
+  },
+  watch: {
+    navigation(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        for (let _attr in this.user) {
+          this.user[_attr] = '';
+        }
+
+        this.$v.$reset();
+      }
+    },
+  },
+};
